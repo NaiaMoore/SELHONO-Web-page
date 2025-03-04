9fcae009e03bf30952d23608a7efa172dee10c23
Naia Moore <nadinhodasevichart@gmail.com>
2025-03-04 17:13:43 +0100

    закончена главная страница добавлена страница About us добавлен слайдер, секции -quote, opinion, work,result начата секция form

---
+++ js/about-us.js
@@ -0,0 +1,93 @@
+
+
+const caruoselRow = document.querySelector('.about-swiper');
+const caruoselSlides = document.getElementsByClassName('slide');
+const dots = document.getElementsByClassName('dot');
+
+const prevBtn = document.querySelector('.btn-prev');
+const nextBtn = document.querySelector('.btn-next');
+
+
+let index = 1;
+var width;
+
+
+function sliderWidth(){
+  width = caruoselSlides[0].clientWidth;
+}
+sliderWidth()
+window.addEventListener('resize', sliderWidth);
+caruoselRow.style.transform = 'translateX('+ (- width * index) + 'px)';
+
+
+
+nextBtn.addEventListener("click", nextSlide);
+function nextSlide(){
+  if(index >= caruoselSlides.length - 1){return};
+  caruoselRow.style.transition = "transform 0.4s ease-out";
+  index++;
+  caruoselRow.style.transform = "translateX("+ (- width * index) + "px)";
+  dotsLabel();
+ }
+
+
+prevBtn.addEventListener("click", prevSlide);
+function prevSlide(){
+  if(index <= 0){return};
+  caruoselRow.style.transition = "transform 0.4s ease-out";
+  index--;
+  caruoselRow.style.transform = "translateX("+ (- width * index) + "px)";
+  dotsLabel();
+
+ }
+
+//  возвращение на первый слайд после последнего слайда
+caruoselRow.addEventListener('transitionend', function(){
+  if(caruoselSlides[index].id==='firstImageDuplicate'){
+    caruoselRow.style.transition = 'none';
+    index = caruoselSlides.length - index;
+    caruoselRow.style.transform = "translateX("+ (- width * index) + "px)";
+    dotsLabel();
+
+  }
+
+  if(caruoselSlides[index].id==='lastImageDuplicate'){
+    caruoselRow.style.transition = 'none';
+    index = caruoselSlides.length - 2;
+    caruoselRow.style.transform = "translateX("+ (- width * index) + "px)";
+    dotsLabel();
+
+  }
+})
+
+//  auto sliding
+
+function autoSlide(){
+deleteInterval = setInterval(timer, 4000);
+function timer(){
+  nextSlide()
+}
+}
+autoSlide();
+
+//Stop auto sliding whem mouse is over
+
+const mainContainer = document.querySelector('.corusel-wrapper')
+
+mainContainer.addEventListener('mouseover', function(){
+  clearInterval(deleteInterval);
+})
+
+//resume sliding when mouse is out 
+mainContainer.addEventListener('mouseout', autoSlide);
+
+
+function dotsLabel(){
+  for(i = 0; i < dots.length; i++){
+    dots[i].className = dots[i].className.replace(' active--dot', '');
+  }
+  dots[index - 1].className += ' active--dot';
+}
+
+
+
